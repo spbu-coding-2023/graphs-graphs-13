862bce4736427567b61618fb2bf746004ce65560
Ekaterina Tenyaeva <b08lsoai@gmail.com>
2024-05-28 15:55:25 +0300

    docs: added documentation to Kosaraju's algorithm

--- src/main/kotlin/model/algorithms/Kosaraju.kt
+++ src/main/kotlin/model/algorithms/Kosaraju.kt
@@ -2,11 +2,23 @@ package model.algorithms
 
 import model.graph.DirectedGraph
 
+/**
+ * The Kosaraju class implements the Kosaraju algorithm for finding strongly connected
+ * components (scc) in a directed graph.
+ * @property graph is a directed graph in which we are looking for scc
+ * @property visited map storing information about visited vertices
+ * @property stack a stack for storing vertices in the order in which they were traversed
+ * @property stronglyConnectedComponents list of strongly connected components in graph
+ */
 class Kosaraju<D>(private val graph: DirectedGraph<D>) {
   private val visited = hashMapOf<Int, Boolean>()
   private val stack = mutableListOf<Int>()
   private val stronglyConnectedComponents = mutableListOf<List<Int>>()
 
+  /**
+   * The findStronglyConnectedComponents method runs an algorithm to find a scc.
+   * @return list of graph components
+   */
   fun findStronglyConnectedComponents(): List<List<Int>> {
     for (vertexID in graph.vertices.keys) {
       if (visited[vertexID] != true) {
@@ -26,6 +38,12 @@ class Kosaraju<D>(private val graph: DirectedGraph<D>) {
     return stronglyConnectedComponents
   }
 
+  /**
+   * The dfs method performs a depth-first traversal of the graph, starting from a given vertex.
+   * @param vertexID identifier of the vertex from which the traversal begins
+   * @param stack for storing vertices in traversed order
+   * @param graph graph in which the traversal occurs
+   */
   private fun dfs(vertexID: Int, stack: MutableList<Int>, graph: DirectedGraph<D>) {
     visited[vertexID] = true
     for (nextVertexID in (graph.adjacency[vertexID]?.keys ?: emptyList()))
@@ -35,6 +53,10 @@ class Kosaraju<D>(private val graph: DirectedGraph<D>) {
     stack.add(vertexID)
   }
 
+  /**
+   * The transposeGraph method transposes the graph.
+   * @return the transposed graph
+   */
   private fun transposeGraph(): DirectedGraph<D> {
     val transposedGraph = DirectedGraph<D>()
     for ((id, vertex) in graph.vertices) {
