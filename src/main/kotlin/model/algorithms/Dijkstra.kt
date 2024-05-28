279b0c674b67e52039c95371a64283318d94cacd
Ekaterina Tenyaeva <b08lsoai@gmail.com>
2024-05-28 15:23:54 +0300

    docs: added documentation to Dijkstra's algorithm

--- src/main/kotlin/model/algorithms/Dijkstra.kt
+++ src/main/kotlin/model/algorithms/Dijkstra.kt
@@ -3,10 +3,26 @@ package model.algorithms
 import model.graph.*
 import java.util.PriorityQueue
 
+/**
+ * The Dijkstra class is used to find the shortest paths in a weighted graph using Dijkstra's algorithm.
+ *
+ * @property graph A directed graph in which shortest paths are searched.
+ * @property distances Stores the lengths of the shortest paths from the starting vertex to other vertices.
+ * @property previousVertices Stores previous vertices along the shortest path to each vertex.
+ * @property queue Queue for processing vertices in order of increasing distances.
+ */
 class Dijkstra<D>(private val graph: Graph<D>) {
   private val distances = hashMapOf<Int, Int>()
   private val previousVertices = hashMapOf<Int, Int?>()
   private val queue = PriorityQueue<Pair<Int, Int>>(compareBy { it.second })
+
+  /**
+   * Finds the shortest path from one vertex (start) to another vertex (end) and returns a list of id vertices of the path.
+   *
+   * @param start The starting vertex from which to start searching for the shortest path.
+   * @param end The end vertex to which the shortest path is sought.
+   * @return A list of vertices that form the shortest path from vertex start to vertex end.
+   */
   fun findShortestPaths(start: Int, end: Int): List<Int> {
     if (!graph.vertices.contains(start) || !graph.vertices.contains(end)) {
       throw NoSuchElementException("The vertex doesn't exist in the graph.")
@@ -32,6 +48,7 @@ class Dijkstra<D>(private val graph: Graph<D>) {
         }
       }
     }
+    // construct final path using previous vertices
     val path = mutableListOf<Int>()
     previousVertices[end] ?: return path
     var current = end
