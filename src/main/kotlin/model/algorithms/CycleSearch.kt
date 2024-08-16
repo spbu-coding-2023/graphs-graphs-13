57f7caa45aa7cd32244f411343d738fa5b6a1a05
Migunova Anastasia <anastasiamigunova76@gmail.com>
2024-08-16 18:55:03 +0300

    feat: added KDoc documentation for CycleSearch algorithm

--- src/main/kotlin/model/algorithms/CycleSearch.kt
+++ src/main/kotlin/model/algorithms/CycleSearch.kt
@@ -4,8 +4,31 @@ import model.graph.Edge
 import model.graph.UndirectedGraph
 import model.graph.Vertex
 
+/**
+ * The class [CycleSearch] implements the algorithm for finding a cycle around
+ * selected vertex of the undirected graph.
+ *
+ * At the same time, the algorithm has some minimization of the desired cycle
+ * by iterating through possible pairs of neighbors of the selected vertex
+ * through which the desired cycle will pass.
+ * @param D input type
+ * @property [graph] a undirected graph for whose vertex we will search for a cycle
+ * @constructor Creates a graph, based on [graph],for which it will be possible to apply
+ * a cycle search algorithm around the vertex selected in it.
+ */
+
 class CycleSearch<D>(private val graph: UndirectedGraph<D>) {
 
+    /**
+     * This auxiliary function for the function [findAnyCycle] aims to get from
+     * some hashmap with extra elements the hashmap the elements of which
+     * accurately describe found cycle path.
+     *
+     * @param cyclePath hashmap with extra elements
+     * @param cycleVertexId the index of the vertex around which the cycle must be found
+     * @return cycle path
+     * @receiver private fun [findAnyCycle]
+     */
     private fun getCyclePath(cyclePath: HashMap<Int, Int>, cycleVertexId: Int): HashMap<Int, Int> {
 
         var current: Int = cycleVertexId
@@ -21,6 +44,17 @@ class CycleSearch<D>(private val graph: UndirectedGraph<D>) {
 
     }
 
+    /**Searches for a cycle by dfs algorithm and writes it into [cyclePath]
+     *
+     *
+     * @param cycleVertexId the index of the vertex around which the cycle must be found
+     * @param currentVertexId using for recording the [cyclePath]
+     * @param visited stores information about which vertices of the graph
+     * have already been processed by [dfs]
+     * @param cyclePath hashmap consisting of elements that can be used to restore the found cycle path
+     * by applying the auxiliary function [getCyclePath] to it in function [findAnyCycle].
+     * @receiver private fun [findAnyCycle]
+     */
     private fun dfs(
         cycleVertexId: Int,
         currentVertexId: Int,
@@ -41,6 +75,12 @@ class CycleSearch<D>(private val graph: UndirectedGraph<D>) {
         }
     }
 
+    /** Finds any existing cycle in the graph around a given vertex
+     *
+     * @param vertexId the index of the vertex around which the cycle must be found
+     * @return found cycle path or null if the cycle doesn't exist around vertex with id = [vertexId]
+     * @receiver fun [findCycle]
+     */
     private fun findAnyCycle(vertexId: Int): HashMap<Int, Int>? {
 
         val devCyclePath = HashMap<Int, Int>()
@@ -57,7 +97,19 @@ class CycleSearch<D>(private val graph: UndirectedGraph<D>) {
         return getCyclePath(devCyclePath, vertexId)
 
     }
-    
+
+    /**
+     * The function implements the algorithm for finding a cycle around
+     * vertex with id = [vertex] of the undirected graph.
+     *
+     * The function searches for a cycle and performs some minimization of it.
+     * During the cycle minimization we iterate through variants of cycle,
+     * based on the choice of a pair of neighbors of [vertex] through which the cycle will pass.
+     * Each of the found cycle variants is written to a variable 'currentCyclePath'
+     * and its size compared with 'minCycleSize'.
+     * @param vertex the vertex around which the cycle must be found
+     * @return cycle path or null if it doesn't exist
+     */
     fun findCycle(vertex: Vertex<D>): UndirectedGraph<D>? {
 
         var returnCyclePath = hashMapOf<Int, Int>()
