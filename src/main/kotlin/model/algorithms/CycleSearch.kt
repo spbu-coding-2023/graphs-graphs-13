af644788c3268d1e8984bf55be563ff46f3a69d8
Migunova Anastasia <anastasiamigunova76@gmail.com>
2024-08-16 17:12:40 +0300

    remade CycleSearch algorithm

--- src/main/kotlin/model/algorithms/CycleSearch.kt
+++ src/main/kotlin/model/algorithms/CycleSearch.kt
@@ -1,68 +1,110 @@
 package model.algorithms
 
-import model.graph.Graph
 import model.graph.Edge
+import model.graph.UndirectedGraph
+import model.graph.Vertex
 
-class CycleSearch<D>(private val graph: Graph<D>) {
+class CycleSearch<D>(private val graph: UndirectedGraph<D>) {
 
-    fun findCycle(cycleVertexId: Int): List<Edge<D>>? {
+    private fun getCyclePath(cyclePath: HashMap<Int, Int>, cycleVertexId: Int): HashMap<Int, Int> {
 
-        if (cycleVertexId !in graph.vertices.keys) {
-            throw IllegalArgumentException("Vertex with index = $cycleVertexId doesn't exist in the graph. The cycle can't be found")
-        }
+        var current: Int = cycleVertexId
+        var next: Int
+        val returnCyclePath = HashMap<Int, Int>()
+        do {
+            next = cyclePath.filter { it.key == current }.values.first()
+            returnCyclePath[current] = next
+            current = next
+        } while (current != cycleVertexId)
 
-        val visited = HashMap<Int, Boolean>()
-        for (vertexId in graph.vertices.keys) {
-            visited[vertexId] = false
-        }
-        val cycleWayFrom =
-            HashMap<Int, Int>()
-        val cycleIsFound = Array<Boolean>(1) { false }
-        val returnList = mutableListOf<Edge<D>>()
-
-        dfs(cycleVertexId, visited, cycleWayFrom, cycleVertexId, cycleIsFound, returnList)
-        if (cycleIsFound[0]) {
-            return returnList
-        }
-        return null
+        return returnCyclePath
 
     }
 
     private fun dfs(
-        vertexId: Int,
-        visited: HashMap<Int, Boolean>,
-        cycleWayFrom: HashMap<Int, Int>,
         cycleVertexId: Int,
-        cycleIsFound: Array<Boolean>,
-        returnList: MutableList<Edge<D>>
+        currentVertexId: Int,
+        visited: HashMap<Int, Boolean>,
+        cyclePath: HashMap<Int, Int>
     ) {
 
-        visited[vertexId] = true
+        for (idAdjacency in graph.adjacency[currentVertexId]!!.keys) {
 
-        if (!cycleIsFound[0]) {
-            for (adjacencyVertexId in graph.adjacency[vertexId]!!.keys) {
-
-                if (visited[adjacencyVertexId] == false) {
-                    cycleWayFrom[adjacencyVertexId] = vertexId
-                    dfs(adjacencyVertexId, visited, cycleWayFrom, cycleVertexId, cycleIsFound, returnList)
-                } else if (adjacencyVertexId == cycleVertexId && adjacencyVertexId != cycleWayFrom[vertexId]) {
-                    cycleWayFrom[cycleVertexId] = vertexId
-                    var id = cycleWayFrom.keys.last()
-                    while (id != cycleVertexId) {
-                        returnList.add(Edge<D>(cycleWayFrom[id]!! to id, graph.adjacency[cycleWayFrom[id]]!![id]))
-                        id = cycleWayFrom[id]!!
-                    }
-                    returnList.add(Edge<D>(cycleWayFrom[id]!! to id, graph.adjacency[cycleWayFrom[id]]!![id]))
-                    cycleIsFound[0] = true
-                }
-
+            if (visited[idAdjacency] == false) {
+                visited[idAdjacency] = true
+                cyclePath[currentVertexId] = idAdjacency
+                dfs(cycleVertexId, idAdjacency, visited, cyclePath)
+            } else if (idAdjacency == cycleVertexId && cyclePath[cycleVertexId] != currentVertexId) {
+                cyclePath[currentVertexId] = idAdjacency
+                return
             }
         }
     }
 
+    private fun findAnyCycle(vertexId: Int): HashMap<Int, Int>? {
+
+        val devCyclePath = HashMap<Int, Int>()
+        val visited = hashMapOf<Int, Boolean>()
+        for (idVertex in graph.vertices.keys) {
+            visited[idVertex] = false
+        }
+        visited[vertexId] = true
+        dfs(vertexId, vertexId, visited, devCyclePath)
+
+        if (devCyclePath.filter { it.value == vertexId }.isEmpty()) {
+            return null
+        }
+        return getCyclePath(devCyclePath, vertexId)
+
+    }
+    
+    fun findCycle(vertex: Vertex<D>): UndirectedGraph<D>? {
+
+        var returnCyclePath = hashMapOf<Int, Int>()
+        var currentCyclePath: HashMap<Int, Int>?
+        var minCycleSize: Int = Int.MAX_VALUE
+
+        if (vertex.id !in graph.vertices.keys) {
+            throw IllegalArgumentException("Vertex with id = ${vertex.id} doesn't exist in the graph")
+        }
+        val returnGraph = UndirectedGraph<D>()
+        if (graph.adjacency[vertex.id]!!.size < 2) {
+            return null
+        } else { // we consider all possible cases of a cycle by choosing a pair of neighbors to minimize the found cycle
+            val adjacencyOfVertex: MutableList<Int> = arrayListOf()
+            graph.adjacency[vertex.id]!!.keys.forEach { adjacencyOfVertex.add(it) }
+            val adjacencyWas: MutableList<Int> = arrayListOf()
+            val removedEdges: MutableList<Edge<Int>> = arrayListOf()
+            for (firstAdjacency in adjacencyOfVertex) {
+                adjacencyWas.add(firstAdjacency)
+                for (secondAdjacency in adjacencyOfVertex.filter { it !in adjacencyWas && it != vertex.id }) {
+
+                    for (adjacency in graph.adjacency[vertex.id]!!.filter { it.key != firstAdjacency && it.key != secondAdjacency }) {
+                        removedEdges.add(Edge(vertex.id to adjacency.key, adjacency.value))
+                        graph.removeEdge(vertex.id to adjacency.key, adjacency.value)
+                    }
+
+                    currentCyclePath = findAnyCycle(vertex.id)
+                    if (currentCyclePath != null && currentCyclePath.size < minCycleSize) {
+                        minCycleSize = currentCyclePath.size
+                        returnCyclePath = currentCyclePath
+                    }
+
+                    for (edge in removedEdges) {
+                        graph.addEdge(edge.vertices, edge.weight)
+                    }
+                    removedEdges.clear()
+                }
+            }
+        }
+
+        if (minCycleSize == Int.MAX_VALUE) {
+            return null
+        }
+        returnCyclePath.forEach { returnGraph.addVertex(it.key, graph.vertices[it.key]!!.data) }
+        returnCyclePath.forEach { returnGraph.addEdge(it.key to it.value, graph.adjacency[it.key]!![it.value]) }
+        return returnGraph
+
+    }
 
 }
-
-
-
-
