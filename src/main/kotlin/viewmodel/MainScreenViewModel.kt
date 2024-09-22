77d737a2df7aa776bbbfc6fcb88bc2798a0e5efe
Ekaterina Tenyaeva <b08lsoai@gmail.com>
2024-09-23 00:10:56 +0300

    feat: added display of Dijkstra's algorithm

--- src/main/kotlin/viewmodel/MainScreenViewModel.kt
+++ src/main/kotlin/viewmodel/MainScreenViewModel.kt
@@ -5,8 +5,11 @@ import androidx.compose.ui.graphics.Color
 import model.graph.Graph
 import viewmodel.graph.GraphViewModel
 import viewmodel.graph.RepresentationStrategy
+import model.algorithms.*
+import model.graph.DirectedGraph
+import model.graph.UndirectedGraph
 
-class MainScreenViewModel<D>(graph: Graph<D>, private val representationStrategy: RepresentationStrategy) {
+class MainScreenViewModel<D>(private val graph: Graph<D>, private val representationStrategy: RepresentationStrategy) {
   val showVerticesLabels = mutableStateOf(false)
   val showEdgesLabels = mutableStateOf(false)
   val graphViewModel = GraphViewModel(graph, showVerticesLabels, showEdgesLabels)
@@ -19,5 +22,14 @@ class MainScreenViewModel<D>(graph: Graph<D>, private val representationStrategy
   fun resetGraphView() {
     representationStrategy.place(800.0, 600.0, graphViewModel.verticesViewValues)
     graphViewModel.verticesViewValues.forEach { v -> v.color = Color.Gray }
+  }
+
+  fun runDijkstraAlgorithm(start: Int, end: Int) {
+    resetGraphView()
+    val dijkstra = Dijkstra(graph)
+    val result = dijkstra.findShortestPaths(start, end)
+    for (vertexId in result) {
+      graphViewModel.verticesView[vertexId]?.color = Color(240, 128, 128)
+    }
   }
 }
