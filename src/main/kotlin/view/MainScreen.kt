507aa693c9f92373f9934c548465bf6256c49748
Ekaterina Tenyaeva <b08lsoai@gmail.com>
2024-09-23 00:46:29 +0300

    feat: added working button for Dijkstra algorithm
    pressing the button runs Dijkstra's algorithm

--- src/main/kotlin/view/MainScreen.kt
+++ src/main/kotlin/view/MainScreen.kt
@@ -2,6 +2,7 @@ package view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -10,6 +11,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.sp
 import model.graph.Graph
 import org.jetbrains.skia.impl.Stats.enabled
@@ -21,6 +23,7 @@ import viewmodel.graph.GraphViewModel
 @Composable
 fun <D> MainScreen(viewModel: MainScreenViewModel<D>) {
   var expanded by remember { mutableStateOf(false) }
+  var showDialog by remember { mutableStateOf(false) }
   Row(
     horizontalArrangement = Arrangement.spacedBy(20.dp)
   ) {
@@ -59,6 +62,7 @@ fun <D> MainScreen(viewModel: MainScreenViewModel<D>) {
         ) {
           DropdownMenuItem(onClick = {
             expanded = false
+            showDialog = true
           }) {
             Text("Dijkstra")
           }
@@ -74,6 +78,15 @@ fun <D> MainScreen(viewModel: MainScreenViewModel<D>) {
           }
         }
       }
+    }
+    if (showDialog) {
+      DijkstraDialog(
+        onDismiss = { showDialog = false },
+        onRunAlgorithm = { start, end ->
+          viewModel.runDijkstraAlgorithm(start, end)
+          showDialog = false
+        }
+      )
     }
     Box {
       var expandedThemeMenu by remember { mutableStateOf(false) }
@@ -112,4 +125,57 @@ fun <D> MainScreen(viewModel: MainScreenViewModel<D>) {
       GraphView(viewModel.graphViewModel)
     }
   }
+}
+@Composable
+fun DijkstraDialog(onDismiss: () -> Unit, onRunAlgorithm: (Int, Int) -> Unit) {
+  var start by remember { mutableStateOf("") }
+  var end by remember { mutableStateOf("") }
+
+  AlertDialog(
+    onDismissRequest = onDismiss,
+    title = {
+      Text("Select vertices to find shortest path")
+    },
+    text = {
+      Column(modifier = Modifier.padding(16.dp)) {
+        TextField(
+          value = start,
+          onValueChange = { start = it },
+          label = { Text("Enter the id of the starting vertex:") },
+          keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+          modifier = Modifier.padding(bottom = 12.dp)
+        )
+        TextField(
+          value = end,
+          onValueChange = { end = it },
+          label = { Text("Enter the id of the destination vertex:") },
+          keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+          modifier = Modifier.padding(bottom = 12.dp)
+        )
+      }
+    },
+    confirmButton = {
+      Button(
+        onClick = {
+          val startInt = start.toIntOrNull()
+          val endInt = end.toIntOrNull()
+
+          if (startInt != null && endInt != null) {
+            onRunAlgorithm(startInt, endInt)
+          }
+        },
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color(139, 0, 0))
+      ) {
+        Text("Find the way", color = Color(255, 250, 250))
+      }
+    },
+    dismissButton = {
+      Button(
+        onClick = onDismiss,
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color(139, 0, 0))
+      ) {
+        Text("Cancel", color = Color(255, 250, 250))
+      }
+    }
+  )
 }
