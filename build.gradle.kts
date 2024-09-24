646c89b05e912b6c99739eb01b3796e15e229118
Ekaterina Tenyaeva <b08lsoai@gmail.com>
2024-09-24 18:10:19 +0300

    feat: dependencies added

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
   // (in a separate module for demo project and in testMain).
   // With compose.desktop.common you will also lose @Preview functionality
   implementation(compose.desktop.currentOs)
+  implementation(compose.material3)
+  implementation(compose.materialIconsExtended)
   testImplementation("org.jetbrains.kotlin:kotlin-test")
   testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.0")
 }
